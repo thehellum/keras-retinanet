3c976e5a7bb85899f869c3d5643a159ff9956e6c
Thomas Hellum <thomas.s.hellum@gmail.com>
2019-07-08 20:38:06 +0200

    add more installation dependencies

--- SW_setup.sh
+++ SW_setup.sh
@@ -5,9 +5,7 @@ RD='\033[0;31m' # Red
 BL='\033[0;34m' # Blue
 NC='\033[0m'    # No Color
 
-IN_DIR=$(pwd)
-
-
+IN_DIR="$(pwd)"
 
 # Check if sudo
 # if [ "$EUID" -ne 0 ]
@@ -15,34 +13,44 @@ IN_DIR=$(pwd)
 #   exit
 # fi
 
-clear
 
 # -- return to home directory --
 cd
 
 # --- update ---
+printf "${BL}[i] Update:${NC}\n"
 
 sudo apt-get update
 sudo apt-get upgrade
 
 # --- if this script has been obtained without git ---
 
+printf "${BL}[i] Installing git if not previously installed:${NC}\n"
+
 sudo apt-get install git-core
 
 # --- setup pip and virtual enviroment ---
 
+printf "${BL}[i] Installing virtual enviroment:${NC}\n"
+
 sudo apt install python3-dev python3-pip
-sudo pip3 install -U virtualenv  # system-wide install
+sudo pip3 install -U virtualenv # system-wide install
+
+printf "${BL}[i] Setting up virtual enviroment:${NC}\n"
 
 virtualenv --system-site-packages -p python3 ./venv
-source ./venv/bin/activate  # sh, bash, ksh, or zsh
+source ./venv/bin/activate 
+
+printf "${BL}[i] Installing pip:${NC}\n"
+
 pip install --upgrade pip
-pip3 install --upgrade pip3
+pip3 install --upgrade pip
 
 # --- install tensorflow ---
 
+printf "${BL}[i] Installing tensorflow and other necessary dependencies:${NC}\n"
+
 pip install --upgrade tensorflow
-pip3 install --upgrade tensorflow
 
 # --- install div ---
 
@@ -58,7 +66,11 @@ pip3 install pandas --user
 
 # --- install associated dependencies ---
 
-cd IN_DIR
+printf "${BL}[i] Install associated dependencies specific to this repo:${NC}\n"
+
+cd ${IN_DIR}
 
 pip install . --user
 pip3 install . --user
+
+python setup.py build_ext --inplace
